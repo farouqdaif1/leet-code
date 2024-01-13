bfd3c9354bd97b839ff9c22f868034c46a25d60f
Farouq Daif <daiffarouq@gmail.com>
2024-01-13 21:46:12 +0200

    Time: 59 ms (24.19%), Space: 43.7 MB (5.18%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if(!head || !head.next) return head
+    let dummy = new ListNode();
+    let prev=dummy
+    let curr= head
+    while(curr&&curr.next){
+     prev.next=curr.next;
+     curr.next=prev.next.next
+     prev.next.next=curr
+     prev=curr;
+     curr=curr.next;
+    }
+    console.log(dummy.next)
+    return dummy.next
+    
+};
