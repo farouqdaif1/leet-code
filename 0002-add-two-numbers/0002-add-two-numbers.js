036d1a16079f71d1aea36ea59e918497bcb11111
Farouq Daif <daiffarouq@gmail.com>
2024-01-13 02:19:44 +0200

    Time: 106 ms (18.86%), Space: 47.9 MB (17.96%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let curr1=l1
+    let curr2=l2
+    let result =new ListNode();
+    let curR=result
+    let temp=0;
+    while(curr1||curr2){
+        let deg1=(curr1)?curr1.val:0
+        let deg2=(curr2)?curr2.val:0
+        let res =deg1+deg2+temp
+        if(res>9){
+            let sRes=res.toString()
+            result.next=new ListNode(parseInt(sRes[1]))
+            temp=parseInt(sRes[0]);
+        }else{
+            result.next=new ListNode(res)
+            temp=0
+        }
+      (curr1)?curr1=curr1.next:null;
+      (curr2)?curr2=curr2.next:null;
+      result=result.next
+    } 
+    curR.val=null
+    if(temp){
+        result.next=new ListNode(temp)
+    }
+   return(curR.next);
+
+};
