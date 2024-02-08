1b60a69f32d91494e88669665d91f2fcf51c4013
Farouq Daif <daiffarouq@gmail.com>
2024-02-08 20:17:45 +0200

    Time: 70 ms (40.13%), Space: 52.5 MB (11.68%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,10 +5,11 @@
 var removeDuplicates = function(nums) {
     let j=1;
     for(let i=1;i<nums.length;i++){
-      if(nums[i]!==nums[i-1]){
-          nums[j]=nums[i]
-          j++
-      }
+        if(nums[i]!=nums[i-1]){
+            nums[j]=nums[i]
+            j++
+        }
     }
-    return j
+    return j;
+
 };
